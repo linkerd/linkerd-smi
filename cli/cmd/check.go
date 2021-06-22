ad2e10633ef9f7c6033e78153c74c127d40be761
Tarun Pothulapati <tarunpothulapati@outlook.com>
2021-06-22 12:33:32 +0530

    Add `linkerd smi check` sub command
    This PR adds a new check sub command which is used to perform
    a check, and make sure that the SMI components are installed
    and functioning correctly.
    
    This follows the same pattern of check that we use in viz, jaeger, etc
    other extensions.
    
    This PR also updates the integration test to use the command, instead
    of doing a `kubectl rollout status`
    
    Signed-off-by: Tarun Pothulapati <tarunpothulapati@outlook.com>

---
+++ cli/cmd/check.go
@@ -0,0 +1,223 @@
+package cmd
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"os"
+	"time"
+
+	"github.com/linkerd/linkerd2/pkg/healthcheck"
+	"github.com/linkerd/linkerd2/pkg/version"
+	"github.com/spf13/cobra"
+)
+
+const (
+
+	// linkerdSMIExtensionCheck adds checks related to the SMI extension
+	linkerdSMIExtensionCheck healthcheck.CategoryID = "linkerd-smi"
+)
+
+var (
+	smiNamespace string
+)
+
+type checkOptions struct {
+	wait      time.Duration
+	output    string
+	proxy     bool
+	namespace string
+}
+
+func smiCategory(hc *healthcheck.HealthChecker) *healthcheck.Category {
+
+	checkers := []healthcheck.Checker{}
+
+	checkers = append(checkers,
+		*healthcheck.NewChecker("linkerd-smi extension Namespace exists").
+			WithHintAnchor("l5d-smi-ns-exists").
+			Fatal().
+			WithCheck(func(ctx context.Context) error {
+				// Get SMI Extension Namespace
+				ns, err := hc.KubeAPIClient().GetNamespaceWithExtensionLabel(ctx, smiExtensionName)
+				if err != nil {
+					return err
+				}
+				smiNamespace = ns.Name
+				return nil
+			}))
+
+	checkers = append(checkers,
+		*healthcheck.NewChecker("SMI extension service account exists").
+			WithHintAnchor("l5d-smi-sc-exists").
+			Fatal().
+			Warning().
+			WithCheck(func(ctx context.Context) error {
+				// Check for Collector Service Account
+				return healthcheck.CheckServiceAccounts(ctx, hc.KubeAPIClient(), []string{"smi-adaptor"}, smiNamespace, "")
+			}))
+
+	checkers = append(checkers,
+		*healthcheck.NewChecker("SMI extension pods are injected").
+			WithHintAnchor("l5d-smi-pods-injection").
+			Warning().
+			WithCheck(func(ctx context.Context) error {
+				// Check if SMI Extension pods have been injected
+				pods, err := hc.KubeAPIClient().GetPodsByNamespace(ctx, smiNamespace)
+				if err != nil {
+					return err
+				}
+				return healthcheck.CheckIfDataPlanePodsExist(pods)
+			}))
+
+	checkers = append(checkers,
+		*healthcheck.NewChecker("SMI extension pods are running").
+			WithHintAnchor("l5d-smi-pods-running").
+			Fatal().
+			WithRetryDeadline(hc.RetryDeadline).
+			SurfaceErrorOnRetry().
+			WithCheck(func(ctx context.Context) error {
+				pods, err := hc.KubeAPIClient().GetPodsByNamespace(ctx, smiNamespace)
+				if err != nil {
+					return err
+				}
+
+				// Check for relevant pods to be present
+				err = healthcheck.CheckForPods(pods, []string{"smi-adaptor"})
+				if err != nil {
+					return err
+				}
+
+				return healthcheck.CheckPodsRunning(pods, "")
+
+			}))
+
+	checkers = append(checkers,
+		*healthcheck.NewChecker("SMI extension proxies are healthy").
+			WithHintAnchor("l5d-smi-proxy-healthy").
+			Fatal().
+			WithRetryDeadline(hc.RetryDeadline).
+			SurfaceErrorOnRetry().
+			WithCheck(func(ctx context.Context) error {
+				return hc.CheckProxyHealth(ctx, hc.ControlPlaneNamespace, smiNamespace)
+			}))
+
+	checkers = append(checkers,
+		*healthcheck.NewChecker("SMI extension proxies are up-to-date").
+			WithHintAnchor("l5d-smi-proxy-cp-version").
+			Warning().
+			WithCheck(func(ctx context.Context) error {
+				var err error
+				if hc.VersionOverride != "" {
+					hc.LatestVersions, err = version.NewChannels(hc.VersionOverride)
+				} else {
+					uuid := "unknown"
+					if hc.UUID() != "" {
+						uuid = hc.UUID()
+					}
+					hc.LatestVersions, err = version.GetLatestVersions(ctx, uuid, "cli")
+				}
+				if err != nil {
+					return err
+				}
+
+				pods, err := hc.KubeAPIClient().GetPodsByNamespace(ctx, smiNamespace)
+				if err != nil {
+					return err
+				}
+
+				return hc.CheckProxyVersionsUpToDate(pods)
+			}))
+
+	checkers = append(checkers,
+		*healthcheck.NewChecker("SMI extension proxies and cli versions match").
+			WithHintAnchor("l5d-smi-proxy-cli-version").
+			Warning().
+			WithCheck(func(ctx context.Context) error {
+				pods, err := hc.KubeAPIClient().GetPodsByNamespace(ctx, smiNamespace)
+				if err != nil {
+					return err
+				}
+
+				return healthcheck.CheckIfProxyVersionsMatchWithCLI(pods)
+			}))
+
+	return healthcheck.NewCategory(linkerdSMIExtensionCheck, checkers, true)
+}
+
+func newCheckOptions() *checkOptions {
+	return &checkOptions{
+		wait:   300 * time.Second,
+		output: healthcheck.TableOutput,
+	}
+}
+
+func (options *checkOptions) validate() error {
+	if options.output != healthcheck.TableOutput && options.output != healthcheck.JSONOutput {
+		return fmt.Errorf("Invalid output type '%s'. Supported output types are: %s, %s", options.output, healthcheck.JSONOutput, healthcheck.TableOutput)
+	}
+	return nil
+}
+
+// newCmdCheck generates a new cobra command for the SMI extension.
+func newCmdCheck() *cobra.Command {
+	options := newCheckOptions()
+	cmd := &cobra.Command{
+		Use:   "check [flags]",
+		Args:  cobra.NoArgs,
+		Short: "Check the SMI extension for potential problems",
+		Long: `Check the SMI extension for potential problems.
+
+The check command will perform a series of checks to validate that the SMI
+extension is configured correctly. If the command encounters a failure it will
+print additional information about the failure and exit with a non-zero exit
+code.`,
+		Example: `  # Check that the SMI extension is up and running
+  linkerd smi check`,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return configureAndRunChecks(stdout, stderr, options)
+		},
+	}
+
+	cmd.Flags().StringVarP(&options.output, "output", "o", options.output, "Output format. One of: basic, json")
+	cmd.Flags().DurationVar(&options.wait, "wait", options.wait, "Maximum allowed time for all tests to pass")
+	cmd.Flags().BoolVar(&options.proxy, "proxy", options.proxy, "Also run data-plane checks, to determine if the data plane is healthy")
+	cmd.Flags().StringVarP(&options.namespace, "namespace", "n", options.namespace, "Namespace to use for --proxy checks (default: all namespaces)")
+
+	return cmd
+}
+
+func configureAndRunChecks(wout io.Writer, werr io.Writer, options *checkOptions) error {
+	err := options.validate()
+	if err != nil {
+		return fmt.Errorf("Validation error when executing check command: %v", err)
+	}
+
+	hc := healthcheck.NewHealthChecker([]healthcheck.CategoryID{}, &healthcheck.Options{
+		ControlPlaneNamespace: controlPlaneNamespace,
+		KubeConfig:            kubeconfigPath,
+		KubeContext:           kubeContext,
+		Impersonate:           impersonate,
+		ImpersonateGroup:      impersonateGroup,
+		APIAddr:               apiAddr,
+		RetryDeadline:         time.Now().Add(options.wait),
+		DataPlaneNamespace:    options.namespace,
+	})
+
+	err = hc.InitializeKubeAPIClient()
+	if err != nil {
+		err = fmt.Errorf("Error initializing k8s API client: %s", err)
+		fmt.Fprintln(werr, err)
+		os.Exit(1)
+	}
+
+	hc.AppendCategories(smiCategory(hc))
+
+	success := healthcheck.RunChecks(wout, werr, hc, options.output)
+
+	if !success {
+		os.Exit(1)
+	}
+
+	return nil
+}
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/linkerd/linkerd-smi
 go 1.16
 
 require (
+	github.com/fatih/color v1.9.0
 	github.com/linkerd/linkerd2 v0.0.0-20210617063832-395cc3677e31
 	github.com/servicemeshinterface/smi-sdk-go v0.4.1
 	github.com/sirupsen/logrus v1.7.0
