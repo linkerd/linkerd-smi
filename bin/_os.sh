e96fd930f9d3107731947b34d97bdf0bd55c9d67
Tarun Pothulapati <tarunpothulapati@outlook.com>
2021-07-08 13:14:29 +0530

    build instead for the current OS
    Signed-off-by: Tarun Pothulapati <tarunpothulapati@outlook.com>

--- bin/_os.sh
+++ bin/_os.sh
@@ -4,48 +4,44 @@ set -eu
 
 os() {
   os=$(uname -s)
-  arch=""
   case $os in
     CYGWIN* | MINGW64*)
-      os=windows
+      echo "windows"
       ;;
     Darwin)
-      os=darwin
+      echo "darwin"
       ;;
     Linux)
-      os=linux
-      arch=$(uname -m)
-      case $arch in
-        x86_64)
-          arch=amd64
-          ;;
-        armv8*)
-          arch=arm64
-          ;;
-        aarch64*)
-          arch=arm64
-          ;;
-        armv*)
-          arch=arm
-          ;;
-        amd64|arm64)
-          arch=$arch
-          ;;
-        *)
-          echo "unsupported architecture: $arch" >&2
-          exit 1
-          ;;
-      esac
+      echo "linux"
       ;;
     *)
       echo "unsupported os: $os" >&2
       exit 1
       ;;
   esac
+}
 
-  if [ -n "$arch" ]; then
-    echo "$os-$arch"
-  else
-    echo "$os"
-  fi
+arch() {
+  arch=$(uname -m)
+  case $arch in
+    x86_64)
+      echo "amd64"
+      ;;
+    armv8*)
+      echo "arm64"
+      ;;
+    aarch64*)
+      echo "arm64"
+      ;;
+    armv*)
+      echo "arm"
+      ;;
+    amd64|arm64)
+      echo $arm
+      ;;
+    *)
+      echo "unsupported architecture: $arch" >&2
+      exit 1
+      ;;
+      esac
 }
